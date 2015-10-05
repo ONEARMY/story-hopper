cad20298f1dd03b18646b7583cf96b65f1c362e9
leo <leo@users.noreply.github.com>
2015-10-05 10:06:25 +0200

    Fixed about page and CSS errors

--- page-about.php
+++ page-about.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-	Template Name: About the project
+	Template Name: About the project (new)
 */
 
 get_header();
@@ -18,7 +18,8 @@ get_header();
 
 	<div class="inner">
 		<h1><?php the_title() ?></h1>
-		<iframe width="640" height="360" src="<?php the_field( 'video' ) ?>" frameborder="0" allowfullscreen></iframe>
+		<iframe width="640" height="360" src="//www.youtube.com/embed/<?= explode( '?v=', get_field( 'video' ) )[1] ?>" frameborder="0" allowfullscreen>
+		</iframe>
 	</div>
 
 </section>
