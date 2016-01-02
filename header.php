830ec9b0a8cee3584d2762492734f07c7e50ab27
Leonard Lamprecht <mindrun@icloud.com>
2016-01-02 17:02:33 +0100

    Load site description on the frontend
    This resolves #23.

--- header.php
+++ header.php
@@ -1,48 +1,50 @@
 <!DOCTYPE html>
 <html>
 
-	<head>
+  <head>
 
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			ga('create', 'UA-68425564-1', 'auto');
-			ga('send', 'pageview');
-		</script>
+      ga('create', 'UA-68425564-1', 'auto');
+      ga('send', 'pageview');
+    </script>
 
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<meta property="og:image" content="<?= get_stylesheet_directory_uri(); ?>/images/og.png" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
 
-		<link rel="stylesheet" href="<?= get_stylesheet_uri() ?>">
-		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic,300,300italic" rel="stylesheet" type="text/css">
-		<link rel="shortcut icon" href="<?= get_stylesheet_directory_uri(); ?>/images/favicon.ico" />
+    <meta property="og:image" content="<?= get_stylesheet_directory_uri(); ?>/images/og.png">
 
-		<?php wp_head() ?>
+    <link rel="stylesheet" href="<?= get_stylesheet_uri() ?>">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic,300,300italic" type="text/css">
+    <link rel="shortcut icon" href="<?= get_stylesheet_directory_uri(); ?>/images/favicon.ico">
 
-	</head>
+    <?php wp_head() ?>
 
-	<?php if( get_the_ID() == 26 ): ?>
-		<body class="blue">
-	<?php else: ?>
-		<body>
-	<?php endif; ?>
+  </head>
 
-		<nav id="menu">
-			<h1>Menu</h1>
+  <?php if( get_the_ID() == 26 ): ?>
+    <body class="blue">
+  <?php else: ?>
+    <body>
+  <?php endif; ?>
 
-			<?php
+    <nav id="menu">
+      <h1>Menu</h1>
 
-			$args = [
-				'container' => false,
-				'theme_location' => 'menu'
-			];
+      <?php
 
-			wp_nav_menu( $args );
+      $args = [
+        'container' => false,
+        'theme_location' => 'menu'
+      ];
 
-			?>
+      wp_nav_menu( $args );
 
-		</nav>
+      ?>
+
+    </nav>
